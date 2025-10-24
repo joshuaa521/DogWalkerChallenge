1c43f5e7f864cbcc8fd739449b50084e56d57d33
JoshuaAddanki <joshuaa521@nycstudents.net>
2025-10-24 15:31:17 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
 public class Main {
+
+    public static int read() throws FileNotFoundException {
+        int money = 0;
+        DogWalkCompany[] companies = new DogWalkCompany[1000];
+        File f = new File("Companies.txt");
+        Scanner s = new Scanner(f);
+        int index = 0;
+        while (s.hasNextLine()) {
+            int[] dogs = new int[24];
+            for (int i = 0; i < dogs.length; i++) {
+                dogs[i] = s.nextInt();
+            }
+            companies[index] = new DogWalkCompany(dogs);
+            index++;
+        }
+        DogWalker[] walkers = new DogWalker[(int) Math.pow(10, 4)];
+        f = new File("Walkers.txt");
+        s = new Scanner(f);
+        index = 0;
+        while (s.hasNextLine()) {
+            int dogs = s.nextInt();
+            DogWalkCompany company = companies[s.nextInt()];
+            walkers[index] = new DogWalker(dogs, company);
+            money += walkers[index].dogWalkShift(s.nextInt(), s.nextInt());
+            index++;
+        }
+        return money;
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(read());
+    }
 }
